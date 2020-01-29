367aed1c7ed88c8e967bd4d3a4e8888f34f2189e
brendenrdowd <brenden.dowd@yahoo.com>
2020-01-28 20:50:38 -0800

    Removed node_modules

--- test/app.test.js
+++ test/app.test.js
@@ -7,6 +7,22 @@ describe('GET /app', () => {
     return supertest(app)
       .get('/app')
       .expect(200)
-      .expect('Content-Type', /json/);
+      .expect('Content-Type', /json/)
+      .then(res => {
+        expect(res.body).to.be.an('array')//.that.contains({});
+        expect(res.body).to.have.lengthOf.at.least(1);
+      })
+  })
+  it('should return 400 if incorrect genre input', () => {
+    return supertest(app)
+      .get('/app')
+      .query({ genre: 'RPG' })
+      .expect(400, { message: "genre not found" })
+  })
+  it('should return 400 if incorrect sort input', () => {
+    return supertest(app)
+      .get('/app')
+      .query({ sort: 'Price' })
+      .expect(400, { message: "sort option not found" });
   })
 });
