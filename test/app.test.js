815a42e704729c7ebab3ccc6874e9c4bb980983b
Kevan Zameni <39892438+KevZam@users.noreply.github.com>
2020-01-29 19:30:34 -0800

    tests

--- test/app.test.js
+++ test/app.test.js
@@ -1,28 +1,29 @@
-const supertest = require('supertest');
-const app = require('../app');
-const { expect } = require('chai');
+const supertest = require("supertest");
+const app = require("../app");
+const { expect } = require("chai");
 
-describe('GET /app', () => {
-  it('should return an array of apps', () => {
+// tests
+describe("GET /app", () => {
+  it("should return an array of apps", () => {
     return supertest(app)
-      .get('/app')
+      .get("/app")
       .expect(200)
-      .expect('Content-Type', /json/)
+      .expect("Content-Type", /json/)
       .then(res => {
-        expect(res.body).to.be.an('array')//.that.contains({});
+        expect(res.body).to.be.an("array"); //.that.contains({});
         expect(res.body).to.have.lengthOf.at.least(1);
-      })
-  })
-  it('should return 400 if incorrect genre input', () => {
+      });
+  });
+  it("should return 400 if incorrect genre input", () => {
     return supertest(app)
-      .get('/app')
-      .query({ genre: 'RPG' })
-      .expect(400, { message: "genre not found" })
-  })
-  it('should return 400 if incorrect sort input', () => {
+      .get("/app")
+      .query({ genre: "RPG" })
+      .expect(400, { message: "genre not found" });
+  });
+  it("should return 400 if incorrect sort input", () => {
     return supertest(app)
-      .get('/app')
-      .query({ sort: 'Price' })
+      .get("/app")
+      .query({ sort: "Price" })
       .expect(400, { message: "sort option not found" });
-  })
+  });
 });
